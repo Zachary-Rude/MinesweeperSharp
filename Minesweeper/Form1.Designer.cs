1068a3a36c84d3d23de0a56bf6d0a431025d31b9
Zachary-Rude <zacharyrude10@gmail.com>
2025-07-15 09:32:58 -0700

    Add project files.

---
+++ Minesweeper/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Minesweeper
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.btnRestart = new System.Windows.Forms.Button();
+			this.pnlBody = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.btnRestart);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(381, 76);
+			this.panel1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.label1.Location = new System.Drawing.Point(0, 74);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(381, 2);
+			this.label1.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(313, 17);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(56, 35);
+			this.textBox2.TabIndex = 2;
+			this.textBox2.Text = "0";
+			this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.textBox2.Enter += new System.EventHandler(this.btnRestart_Enter);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(12, 17);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(56, 35);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "30";
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.textBox1.Enter += new System.EventHandler(this.btnRestart_Enter);
+			// 
+			// btnRestart
+			// 
+			this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this.btnRestart.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.btnRestart.Location = new System.Drawing.Point(147, 28);
+			this.btnRestart.Name = "btnRestart";
+			this.btnRestart.Size = new System.Drawing.Size(75, 23);
+			this.btnRestart.TabIndex = 0;
+			this.btnRestart.Text = "Restart";
+			this.btnRestart.UseVisualStyleBackColor = true;
+			this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+			// 
+			// pnlBody
+			// 
+			this.pnlBody.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pnlBody.Location = new System.Drawing.Point(0, 76);
+			this.pnlBody.Name = "pnlBody";
+			this.pnlBody.Size = new System.Drawing.Size(381, 341);
+			this.pnlBody.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(381, 417);
+			this.Controls.Add(this.pnlBody);
+			this.Controls.Add(this.panel1);
+			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "MinesweeperSharp";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button btnRestart;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Panel pnlBody;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ Minesweeper/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1: Form
+    {
+		byte[,] Positions = new byte[15, 15];
+		Button[,] ButtonList = new Button[15, 15];
+
+        public Form1()
+        {
+            InitializeComponent();
+			this.Icon = Properties.Resources.icon;
+			GenerateBombs();
+			GeneratePositionValue();
+			GenerateButtons();
+        }
+
+		Random rnd = new Random();
+		private void GenerateBombs()
+		{
+			int bombs = 0;
+			while (bombs < 30)
+			{
+				int x = rnd.Next(0, 14);
+				int y = rnd.Next(0, 14);
+
+				if (Positions[x,y] == 0)
+				{
+					Positions[x, y] = 10;
+					bombs++;
+				}
+			}
+		}
+
+		private void GeneratePositionValue()
+		{
+			for (int x = 0; x < 15; x++)
+			{
+				for (int y = 0; y < 15; y++)
+				{
+					if (Positions[x, y] == 10)
+						continue;
+					int bombCount = 0;
+					for (int counterX = -1; counterX < 2; counterX++)
+					{
+						int checkerX = x + counterX;
+
+						for (int counterY = -1; counterY < 2; counterY++)
+						{
+							int checkerY = y + counterY;
+
+							if (checkerX == -1 || checkerY == -1 || checkerX > 14 || checkerY > 14)
+								continue;
+
+							if (checkerY == y && checkerX == x)
+								continue;
+
+							if (Positions[checkerX, checkerY] == 10)
+								bombCount++;
+						}
+					}
+
+					if (bombCount == 0)
+						Positions[x, y] = 20;
+					else
+						Positions[x, y] = (byte)bombCount;
+				}
+			}
+		}
+
+		private void GenerateButtons()
+		{
+			int xLoc = 3;
+			int yLoc = 6;
+			for (int x = 0; x < 15; x++)
+			{
+				for (int y = 0; y < 15; y++)
+				{
+					Button btn = new Button();
+					btn.Parent = pnlBody;
+					btn.Location = new Point(xLoc, yLoc);
+					btn.Size = new Size(25, 22);
+					btn.Font = new Font("Consolas", 8.25F);
+					btn.Tag = $"{x},{y}";
+					btn.Click += BtnClick;
+					btn.MouseUp += BtnMouseUp;
+					btn.Enter += btnRestart_Enter;
+					xLoc += 25;
+					ButtonList[x, y] = btn;
+				}
+				yLoc += 22;
+				xLoc = 3;
+			}
+		}
+
+		private void BtnClick(object sender, EventArgs e)
+		{
+			Button btn = (Button)sender;
+
+			int x = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(0));
+			int y = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(1));
+			byte value = Positions[x, y];
+
+			if (value == 10)
+			{
+				btn.Font = new Font("Segoe UI Emoji", 8.25F);
+				btn.Text = "💣";
+
+				pnlBody.Enabled = false;
+				MessageBox.Show(string.Format("Game Over!\r\nYou got {0} {1} and used {2} {3}.", points, (points == 1 ? "point" : "points"), 30 - flag, (30 - flag == 1 ? "flag" : "flags")), "MinesweeperSharp", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			else if (value == 20)
+			{
+				btn.FlatStyle = FlatStyle.Flat;
+				btn.FlatAppearance.BorderSize = 1;
+				btn.Enabled = false;
+				OpenAdjacentEmptyTile(btn);
+				points++;
+			}
+			else
+			{
+				btn.FlatStyle = FlatStyle.Flat;
+				btn.FlatAppearance.BorderSize = 1;
+				btn.FlatAppearance.BorderColor = SystemColors.ControlDark;
+				btn.FlatAppearance.MouseDownBackColor = btn.BackColor;
+				btn.FlatAppearance.MouseOverBackColor = btn.BackColor;
+				btn.Text = Positions[x, y].ToString();
+				switch (Positions[x, y])
+				{
+					case 1:
+						btn.ForeColor = Color.DodgerBlue;
+						break;
+					case 2:
+						btn.ForeColor = Color.ForestGreen;
+						break;
+					case 3:
+						btn.ForeColor = Color.Red;
+						break;
+					case 4:
+						btn.ForeColor = Color.MidnightBlue;
+						break;
+					case 5:
+						btn.ForeColor = Color.DarkRed;
+						break;
+					case 6:
+						btn.ForeColor = Color.LightSeaGreen;
+						break;
+					case 7:
+						btn.ForeColor = SystemColors.ControlText;
+						break;
+					case 8:
+						btn.ForeColor = SystemColors.ControlDarkDark;
+						break;
+					case 9:
+						btn.ForeColor = Color.Violet;
+						break;
+				}
+				points++;
+			}
+			btn.Click -= BtnClick;
+			textBox2.Text = points.ToString();
+		}
+
+		private void OpenAdjacentEmptyTile(Button btn)
+		{
+			int x = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(0));
+			int y = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(1));
+			List<Button> emptyButtons = new List<Button>();
+
+			for (int counterX = -1; counterX < 2; counterX++)
+			{
+				int checkerX = x + counterX;
+
+				for (int counterY = -1; counterY < 2; counterY++)
+				{
+					int checkerY = y + counterY;
+
+					if (checkerX == -1 || checkerY == -1 || checkerX > 14 || checkerY > 14)
+						continue;
+
+					if (checkerY == y && checkerX == x)
+						continue;
+
+					Button btnAdjacent = ButtonList[checkerX, checkerY];
+					int xAdjacent = Convert.ToInt32(btnAdjacent.Tag.ToString().Split(',').GetValue(0));
+					int yAdjacent = Convert.ToInt32(btnAdjacent.Tag.ToString().Split(',').GetValue(1));
+
+					byte value = Positions[xAdjacent, yAdjacent];
+
+					if (value == 20)
+					{
+						if (btnAdjacent.FlatStyle != FlatStyle.Flat)
+						{
+							btnAdjacent.FlatStyle = FlatStyle.Flat;
+							btn.FlatAppearance.BorderSize = 1;
+							btn.FlatAppearance.BorderColor = SystemColors.ControlDark;
+							btnAdjacent.Enabled = false;
+							emptyButtons.Add(btnAdjacent);
+						}
+					}
+					else if (value != 10)
+					{
+						btnAdjacent.PerformClick();
+					}
+				}
+			}
+
+			foreach (var btnEmpty in emptyButtons)
+			{
+				OpenAdjacentEmptyTile(btnEmpty);
+			}
+		}
+
+		int flag = 30;
+
+		int points = 0;
+		private void BtnMouseUp(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Right)
+			{
+				Button btn = (Button)sender;
+				if (string.IsNullOrEmpty(btn.Text))
+				{
+					btn.Text = "🚩";
+					btn.Font = new Font("Segoe UI Emoji", 8.25F);
+					btn.Click -= BtnClick;
+					flag--;
+				}
+				else if (!int.TryParse(btn.Text, out _))
+				{
+					btn.Text = "";
+					btn.Font = new Font("Consolas", 8.25F);
+					btn.Click += BtnClick;
+					flag++;
+				}
+				textBox1.Text = flag.ToString();
+			}
+		}
+
+		private void btnRestart_Click(object sender, EventArgs e)
+		{
+			points = 0;
+			textBox2.Text = points.ToString();
+			flag = 30;
+			textBox1.Text = flag.ToString();
+
+			for (int x = 0; x < 15; x++)
+			{
+				for (int y = 0; y < 15; y++)
+				{
+					ButtonList[x, y].Dispose();
+				}
+			}
+
+			pnlBody.Enabled = true;
+			pnlBody.Controls.Clear();
+			ButtonList = new Button[15, 15];
+			Positions = new byte[15, 15];
+
+			GenerateBombs();
+			GeneratePositionValue();
+			GenerateButtons();
+		}
+
+		private void btnRestart_Enter(object sender, EventArgs e)
+		{
+			label1.Focus();
+		}
+	}
+}
